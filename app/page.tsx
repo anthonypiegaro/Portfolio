8c8839a3e945980d217db72b8602c752495ea69a
Anthony Piegaro <84252391+anthonypiegaro@users.noreply.github.com>
2025-04-24 23:29:03 +0000

    Add Page Layout with Animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
+import { useRef } from "react"
+import { motion, useScroll, useTransform } from "motion/react"
 import { Quicksand } from "next/font/google"
-import { Parallax, ParallaxLayer } from "@react-spring/parallax"
 
 import { PrettyLink } from "@/components/pretty-link"
 import { DownArrow } from "@/components/down-arrow"
@@ -11,42 +12,69 @@ const quicksand = Quicksand({
 })
 
 export default function Home() {
+  const container = useRef(null)
+
+  const { scrollYProgress } = useScroll({ container: container })
+
+  const downArrowOpacity = useTransform(
+    scrollYProgress,
+    [0, 0.1],
+    [1, 0]
+  )
+
+  const downArrowBottom = useTransform(
+    scrollYProgress,
+    [0, 0.2],
+    ["0%", "50%"]
+  )
+
   return (
-    <main>
-      <header>
-        <nav></nav>
-      </header>
-      <Parallax pages={5} style={{ top: 0, left: 0 }} className="bg-neutral-900">
-        <ParallaxLayer offset={0} speed={0.8}>
-          <div className="w-full h-full flex items-end justify-center pb-15">
-            <DownArrow />
-          </div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.1}>
-          <div className={`w-full h-full mx-auto max-w-prose px-4 py-10 cursor-default sm:px-6 sm:text-lg md:text-xl md:py-15 lg:px-0 lg:py-20 lg:text-2xl ${quicksand.className}`}>
-            <p>
-              Hey,<br/><br/>
-              Welcome to my portfolio. Here you can find a collection of my deployed work.
-              If you would like to contact me, you can fill out the <PrettyLink href="#">form below</PrettyLink>, 
-              or you can reach out to me on <PrettyLink href="#">LinkedIn</PrettyLink>. Thank you for visiting, 
-              I hope you enjoy.<br/><br/>
-              - Anthony Piegaro
-            </p>
-          </div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1}>
-          <div>Project 1</div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={2}>
-          <div>Project 2</div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={3}>
-          <div>Project 3</div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={4}>
-          <div>Contact Info</div>
-        </ParallaxLayer>
-      </Parallax>
+    <main className="w-dvw h-dvh bg-neutral-900 overflow-auto" ref={container} >
+      <div className={`relative w-full h-full mx-auto max-w-prose px-4 pt-10 cursor-default sm:px-6 sm:text-lg md:text-xl md:pt-15 lg:px-0 lg:pt-20 lg:text-2xl ${quicksand.className}`}>
+        <p>
+          Hey,<br/><br/>
+          Welcome to my portfolio. Here you can find a collection of my deployed work.
+          If you would like to contact me, you can fill out the <PrettyLink href="#">form below</PrettyLink>, 
+          or you can reach out to me on <PrettyLink href="#">LinkedIn</PrettyLink>. Thank you for visiting, 
+          I hope you enjoy.<br/><br/>
+          - Anthony Piegaro
+        </p>
+        <motion.div className="absolute left-1/2 -translate-x-1/2" style={{ bottom: downArrowBottom, opacity: downArrowOpacity }} >
+          <DownArrow/>
+        </motion.div>
+      </div>
+      <div className="flex justify-center items-center w-full h-full">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+        >
+          Project 1
+        </motion.div>
+      </div>
+      <div className="flex justify-center items-center w-full h-full">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+        >
+          Project 2
+        </motion.div>
+      </div>
+      <div className="flex justify-center items-center w-full h-full">
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+        >
+          Project 3
+        </motion.div>
+      </div>
+      <div className="flex justify-center items-center w-full h-full">
+        <motion.div>
+          Contact Info
+        </motion.div>
+      </div>
     </main>
   );
 }
--- components/down-arrow.tsx
+++ components/down-arrow.tsx
@@ -1,6 +1,6 @@
 export function DownArrow() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-12">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-12 md:size-14 lg:size-16 xl:size-18">
       <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
     </svg>
   )
